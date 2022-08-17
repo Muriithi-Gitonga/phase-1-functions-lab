8d3ff94c286ad76c385de0339184e3f919e13470
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-08-17 12:34:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks(location) {
-    if (location>42) {
-        return location - 42;
-    }else { 
-        return 42 - location;
-    } 
+    return Math.abs(location -42)
 }
 
 function distanceFromHqInFeet(feet) {
@@ -29,7 +25,7 @@ function calculatesFarePrice(start, destination) {
     if (distanceTravelled < 400) {
         return 0;
     } else if (distanceTravelled > 400 && distanceTravelled < 2000) {
-        return 2.56;
+        return Math.abs(distanceTravelled - 400) * 2/100;
     } else if (distanceTravelled > 2000 && distanceTravelled < 2500) {
         return 25;
     } else if (distanceTravelled > 2500) {
